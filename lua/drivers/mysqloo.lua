1111342ecfc317007238d8d6ee84b6f125d78a48
Gonzalolog <gonzalolog@gmail.com>
2022-02-09 11:58:08 -0300

    Update mysqloo.lua
    Made DBInit run a big later

--- lua/drivers/mysqloo.lua
+++ lua/drivers/mysqloo.lua
@@ -1,16 +1,19 @@
 require("mysqloo");
 local shouldLog = CreateConVar("nebula_logmysql", "0", {FCVAR_ARCHIVE, FCVAR_NOTIFY});
 
-function NebulaDriver:ConnectMySQL()
-    if self.DB and self.DB:status() == mysqloo.DATABASE_CONNECTED then
+function NebulaDriver:ConnectMySQL(force)
+    if not force and self.DB and self.DB:status() == mysqloo.DATABASE_CONNECTED then
         return;
     end
 
     self.DB = mysqloo.connect(NebulaDriver.Config.sql.address, NebulaDriver.Config.sql.user, NebulaDriver.Config.sql.password, NebulaDriver.Config.sql.database, NebulaDriver.Config.sql.port);
     self.DB.onConnected = function()
         MsgC(Color(0, 255, 0), "[Nebula] Connected to MySQL database.\n");
-        hook.Run("DatabaseInitialized");
-        hook.Run("DatabaseCreateTables", self.MySQLCreateTable);
+        timer.Simple(.1, function()
+            hook.Run("DatabaseInitialized");
+            hook.Run("DatabaseCreateTables", self.MySQLCreateTable);
+        end)
+        
     end
 
     self.DB.onConnectionFailed = function(db, err, sql)
@@ -35,7 +38,7 @@ function NebulaDriver:MySQLQuery( sQuery, fSuccess, fFail )
             debug.Trace()
             MsgN(string.rep("-", 16));
         end
-        MsgN("\n[SQL] Query error:\n" .. sQuery)
+        MsgN("\n[SQL] Query error:\n" .. sQuery .. "\n" .. sError .. "\n");
     end
 
     oQuery:start()
@@ -80,7 +83,8 @@ end
 
 local function valToSQL( xVal )
     if isstring( xVal ) and not string.EndsWith( xVal, "()" ) then
-        xVal = "'" .. xVal .. "'"
+        xVal = "'" .. string.Replace( xVal, "'", "" ) .. "'"
+        MsgN(xVal)
         return xVal
     end
 
@@ -146,35 +150,38 @@ function NebulaDriver:MySQLUpdate( sTable, tUpdateTable, sCondition, fCallback )
 end
 
 function NebulaDriver:MySQLPlayer(ply)
-    local header = "SELECT * FROM"
-    local query = ""
-    local first = false
-    for k, call in pairs(self.Joins or {}) do
-        if not first then
-            first = true
-            header = header .. " " .. k .. "\n"
-            continue
-        end
-        query = query .. "\tLEFT JOIN\n\t" .. k .. "\n\tON " .. k .. ".steamid=" .. ply:SteamID64() .. "\n"
-    end
-
-    query = query .. "\tUNION ALL\n" .. header .. string.Replace(query, "LEFT", "RIGHT")
-    query = header .. query
-    self:MySQLQuery(query, function(data)
-        if not IsValid(ply) then return end
-        PrintTable(data)
-        if data and data[1] then
-            for k, v in pairs(self.Joins) do
-                v(ply, data[1])
+    local dataTable = {}
+    local remaining = table.Count(self.Joins)
+    for tbl, call in pairs(self.Joins or {}) do
+        self:MySQLQuery("SELECT * FROM " .. tbl .. " WHERE steamid=" .. ply:SteamID64(), function(data)
+            if (data and data[1]) then
+                for k, v in pairs(data[1]) do
+                    if (k == "steamid") then
+                        continue;
+                    end
+                    if (dataTable[k]) then
+                        MsgC(Color(75, 100, 255), "[MYSQL]", color_white, "Duplicate key " .. k .. " in table " .. tbl .. "\n")
+                    end
+                    dataTable[k] = v
+                end
             end
-        end
-    end)
+
+            remaining = remaining - 1
+
+            if (remaining == 0) then
+                for _, cb in pairs(self.Joins) do
+                    cb(ply, dataTable)
+                end
+            end
+        end)
+    end
 end
 
 function NebulaDriver:MySQLHook(id, callback)
     if not self.Joins then
         self.Joins = {}
     end
+    MsgN("Added driver")
     self.Joins[id] = callback
 end
 
