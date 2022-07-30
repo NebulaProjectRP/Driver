7aa3669cd3b86462a61b5c310d960dccc0e60a8f
Wookie <bq1214@gmail.com>
2022-07-30 22:36:21 +0500

    Added config file

--- lua/autorun/server/nebuladriver.lua
+++ lua/autorun/server/nebuladriver.lua
@@ -1,18 +1,5 @@
 NebulaDriver = NebulaDriver or {}
-NebulaDriver.Config = {
-    sql = {
-        address = "127.0.0.1",
-        user = "root",
-        password = "",
-        port = "3306",
-        database = "nebularp"
-    },
-    redis = {
-        address = "localhost",
-        port = 6379,
-        password = ""
-    }
-}
 
+include("nebula_driver_config.lua")
 include("drivers/mysqloo.lua")
 include("drivers/redis.lua")
