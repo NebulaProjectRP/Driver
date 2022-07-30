cf41209d4ff91b476921512746070b479d45c6ab
Wookie <bq1214@gmail.com>
2022-07-30 22:47:13 +0500

    Populated config file

---
+++ lua/nebula_driver_config.lua
@@ -0,0 +1,14 @@
+NebulaDriver.Config = {
+    sql = {
+        address = "",
+        user = "",
+        password = "",
+        port = "3306",
+        database = ""
+    },
+    redis = {
+        address = "",
+        port = 6379,
+        password = ""
+    }
+}
